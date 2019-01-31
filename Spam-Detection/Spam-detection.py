b7ab2731faa3ba9b69dc5ff0e25d3387852ac6ed
PriyankaH21 <40130156+PriyankaH21@users.noreply.github.com>
2019-01-30 19:37:37 -0500

    Successfully able to detect spam vs non-spam emails

--- Spam-Detection/Spam-detection.py
+++ Spam-Detection/Spam-detection.py
@@ -2,8 +2,35 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
 CORPUS = [
-  'Dear Sir, you have won money $$$$. Come collect your gift',
+  'Dear Sir, you have won a million. Come collect your gift',
   'Do you want to try these pills that make you look better? it only costs 100$',
   'Hi, you have been selected for an interview!',
-  'Sale! Clearance!'
+  'Sale! Clearance!',
+  'Your account has been hacked!',
+  'Hello Sir, I want to inform you that you have won the OLG jackpot',
+  'Please enter your username and password',
+  'Hi Alice, how are you doing today?',
+  'Hi Alice!',
+  'Congratulations!'
+  'How are you?',
+  ''
 ]
+
+# Spam = 0 
+# Non spam = 1
+y = [0,0,1,1,0,0,0,1,1,1,1]
+
+vectorizer = CountVectorizer()
+vectorizer.fit(CORPUS)
+
+x = vectorizer.transform(CORPUS)
+
+classifier = MultinomialNB()
+classifier.fit(x,y)
+
+tests = [
+    "Dear sir, you have won one million money",
+    "Your account has been hacked"
+]
+test_vectors = vectorizer.transform(tests)
+print(classifier.predict(test_vectors))
